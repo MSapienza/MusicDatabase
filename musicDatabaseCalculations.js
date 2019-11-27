58a1f4a24c65aa96e817137fad111a41949e6e0e
MJS <msapienza.28@gmail.com>
2019-11-27 01:10:44 -0600

    Building skeleton for big project

--- musicDatabaseCalculations.js
+++ musicDatabaseCalculations.js
@@ -1,9 +1,26 @@
 
 function validateLogin(username, password)
 {
-
-     //TODO build PHP to retrieve matching user 
+     var user = username + "," + password;
      //TODO redirect to index
-     //Test
+
+     var xhttp;
      
+     if (user == "") 
+     {
+          document.getElementById("txtHint").innerHTML = "";
+          return;
+     }
+     xhttp = new XMLHttpRequest();
+
+     xhttp.onreadystatechange = function() 
+     {
+          if (this.readyState == 4 && this.status == 200)
+          {
+               document.getElementById("txtHint").innerHTML = this.responseText;
+          }
+     };
+
+     xhttp.open("GET", "userValidation.php?q="+user, true);
+     xhttp.send();
 }
---
+++ userValidation.php
@@ -0,0 +1,36 @@
+<?php
+$conn = mysqli_connect("127.0.0.1", "jsuser", "jsuser", "sys");
+if($mysqli->connect_error) 
+{
+  exit('Could not connect');
+}
+
+$q = strval($_GET['q']);
+
+$splitPosition = strpos($q, ",");
+$username;
+$password;
+
+for ($i = 0; $i < $splitPosition; $i++)
+{
+     $username[$i] = $q[$i];
+}
+
+for ($i = $splitPosition, $j = 0; $i < strlen($q); $i++, $j++)
+{
+     $password[$j] = $q[$i];
+}
+
+echo $username;
+echo $password;
+
+$sql = "SELECT * FROM MUSIC_USERS_T WHERE USERNAME = '".$username."' AND PASSWORD = ''".$password."'";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_array($result))
+{
+     echo "logic successful";
+} 
+
+mysqli_close($conn);
+ ?>
